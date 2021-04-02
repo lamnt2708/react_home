e4dc6a177b32f214e0b1d32c7be05e7adb0553ff
bacnp <68533404+bacnp@users.noreply.github.com>
2021-04-02 14:04:25 +0700

    Updates

---
+++ precache-manifest.839d9ab6726c4ecc329c14f21a383704.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3881e945be92786b24273142f2a11a",
+    "url": "/react_home/index.html"
+  },
+  {
+    "revision": "71fba6b79d050dd9c58c",
+    "url": "/react_home/static/css/2.71261bc7.chunk.css"
+  },
+  {
+    "revision": "bff36bf2bedcaadd0995",
+    "url": "/react_home/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "71fba6b79d050dd9c58c",
+    "url": "/react_home/static/js/2.43baa528.chunk.js"
+  },
+  {
+    "revision": "78e572a38ce62fad1af1cd75e03f15bb",
+    "url": "/react_home/static/js/2.43baa528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff36bf2bedcaadd0995",
+    "url": "/react_home/static/js/main.14ecf339.chunk.js"
+  },
+  {
+    "revision": "a67bb24fbec3e25dfa01",
+    "url": "/react_home/static/js/runtime-main.772e703c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react_home/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react_home/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react_home/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react_home/static/media/slick.f97e3bbf.svg"
+  }
+]);
