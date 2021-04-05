8c68a7c503b2c48622a8b0a15527262be64e707c
bacnp <68533404+bacnp@users.noreply.github.com>
2021-04-05 18:03:56 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_home/precache-manifest.980eaaf44c9694522d212a35190c7e95.js"
+  "/react_home/precache-manifest.513c62001ac7710308016d64079167c2.js"
 );
 
 self.addEventListener('message', (event) => {
